aeac3e703f26f912f0225cc024dccd32182fafb1
jmlagumbay <lagumbaj@sheridancollege.ca>
2026-02-05 12:02:36 -0500

    Update foldername from java/com/mycompany /principleexample to cardgame

--- src/main/java/cardgame/PrincipleExample.java
+++ src/main/java/cardgame/PrincipleExample.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.principleexample;
+package cardgame;
 
 public class PrincipleExample {
     public static void main(String[] args) {
