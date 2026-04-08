8578a6bab51a7c15e59ae090393e17f31c9c83be
jmlagumbay <lagumbaj@sheridancollege.ca>
2026-04-07 21:08:06 -0400

    Added main

---
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author Group 2 - Bruins (Ria Gahlot, Madison Scarlett, Rayyan Javed, John Michael Lagumbay)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Blackjack game = new Blackjack();   
+        game.play();
+    }
+}
