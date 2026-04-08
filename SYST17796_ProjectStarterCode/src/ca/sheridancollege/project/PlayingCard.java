3e41fba6a82b2afa3c954ecbe6facb6dacadf104
RJ <RJ@RJ-LAPTOP>
2026-04-07 22:47:09 -0400

    define special attributes of a card, including a method to return the blackjack value

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/PlayingCard.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/PlayingCard.java
@@ -1,53 +1,60 @@
 package ca.sheridancollege.project;
 
-import ca.sheridancollege.project.DeckOfCards.*;
-
+/**
+ * A concrete class representing a standard playing card.
+ */
 public class PlayingCard extends Card {
+    // Reuse enums from DeckOfCards
+    private final DeckOfCards.Rank rank;
+    private final DeckOfCards.Suit suit;
 
-	public String Suit;
-	public String Rank;
-	private Suit suit;
-	private Rank rank;
-	private int value;
+    public PlayingCard(DeckOfCards.Rank rank, DeckOfCards.Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
 
-	public Suit getSuit() {
-		return this.suit;
-	}
+    public DeckOfCards.Rank getRank() {
+        return rank;
+    }
 
-	/**
-	 * 
-	 * @param suit
-	 */
-	public void setSuit(Suit suit) {
-		this.suit = suit;
-	}
+    public DeckOfCards.Suit getSuit() {
+        return suit;
+    }
 
-	public Rank getRank() {
-		return this.rank;
-	}
+    /**
+     * Returns the Blackjack value of the card.
+     * Face cards are 10, Aces are 1
+     */
+    public int getValue() {
+        switch (rank) {
+            case ACE: 
+                return 1;
+            case TWO: 
+                return 2;
+            case THREE: 
+                return 3;
+            case FOUR: 
+                return 4;
+            case FIVE: 
+                return 5;
+            case SIX: 
+                return 6;
+            case SEVEN: 
+                return 7;
+            case EIGHT: 
+                return 8;
+            case NINE: 
+                return 9;
+            case TEN:
+            case JACK:
+            case QUEEN:
+            case KING: return 10;
+            default: return 0;
+        }
+    }
 
-	/**
-	 * 
-	 * @param rank
-	 */
-	public void setRank(Rank rank) {
-		this.rank = rank;
-	}
-
-	public int getValue() {
-		return this.value;
-	}
-
-	/**
-	 * 
-	 * @param value
-	 */
-	public void setValue(int value) {
-		this.value = value;
-	}
-        
-        @Override
-        public String toString() {
-                return "";
-        }    
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
